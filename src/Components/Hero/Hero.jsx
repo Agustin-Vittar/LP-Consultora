17a2b70d10b971b7863985e9327f7b4e72242795
Agustin-Vittar <josevittar99@gmail.com>
2023-10-13 20:11:30 -0300

    Cambio en el título

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Hero.module.css";
 
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 800);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div
       name="hero"
       id="hero"
-      className={`bg-heroimg bg-cover bg-center h-[90vh] flex justify-start sm:justify-start items-start sm:items-center ${styles.imgfondo}`}
+      className={`bg-heroimgsmall sm:bg-heroimg bg-cover bg-center h-[90vh] flex justify-end text-end sm:text-start sm:justify-start items-start sm:items-end ${styles.imgfondo}`}
     >
-      <h1 className={`p-10 ${styles.title}`}>
-        Especialistas en la <br />
-        selección y gestión <br />
-        de talento.
-      </h1>
+      {isMobile ? (
+        <h1 className={`p-5 ${styles.title}`}>
+          Especialistas en <br />
+          la selección y <br /> gestión de
+          <br /> talento.
+        </h1>
+      ) : (
+        <h1 className={`${styles.title}`}>
+          Especialistas en la <br />
+          selección y gestión <br />
+          de talento.
+        </h1>
+      )}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ export default {
       backgroundImage: {
         heroimg:
           "url('https://res.cloudinary.com/djiasfflw/image/upload/v1696857736/Proyecto%20LP/u0refj083o6dmycxkn0h.png')",
+        heroimgsmall:
+          "url('src/assets/Multiracial group of young creative people in smart casual wear discussing business shaking hands together (1).png')",
       },
       colors: {
         primary: "#F9F4F4",
