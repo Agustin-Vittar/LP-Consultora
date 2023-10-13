e571f18d1ec4a40bd2fc2475340516cbd8ed3651
Agustin-Vittar <josevittar99@gmail.com>
2023-10-13 20:21:20 -0300

    Cambio en el título

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         heroimg:
           "url('https://res.cloudinary.com/djiasfflw/image/upload/v1696857736/Proyecto%20LP/u0refj083o6dmycxkn0h.png')",
         heroimgsmall:
-          "url('src/assets/Multiracial group of young creative people in smart casual wear discussing business shaking hands together (1).png')",
+          "url('https://res.cloudinary.com/djiasfflw/image/upload/v1697239231/Proyecto%20LP/kkqdoknp2zjnkxhnvnnd.png')",
       },
       colors: {
         primary: "#F9F4F4",
